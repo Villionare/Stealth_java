3dd94c266e84dc8176700f5bb07427af58107a39
duedull <mr.duedull@gmail.com>
2025-06-17 17:38:11 +0530

    Created a travel Booking App using Custom Exceptions

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
 public class Main {
-
-    public static void main(String[] args) {
-
+    @Test
+    public void validatePaymentOptionTestInvalid() {
+        Bill bill = new Bill("Paypal");
+        // Provide a valid value and observe the JUnit test report
+        Assert.assertFalse(bill.validatePaymentOption());
+    }
+}
+class Bill {
+    private String paymentMode;
+    // other variables and methods
+    public Bill(String paymentMode) {
+        this.paymentMode = paymentMode;
+    }
+    public boolean validatePaymentOption() {
+        if (paymentMode.equals("Credit Card")
+                || paymentMode.equals("Debit Card")
+                || paymentMode.equals("PayPal")
+                || paymentMode.equals("Amazon Pay")
+                || paymentMode.equals("Google Pay")) {
+            return true;
+        }
+        return false;
     }
 }
