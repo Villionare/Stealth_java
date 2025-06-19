f27fdf3e785c47c0da7a750bb7a671a46df11664
duedull <mr.duedull@gmail.com>
2025-06-19 14:48:13 +0530

    Created a functioning alarm clock using threads

--- src/AlarmClock/Main.java
+++ src/AlarmClock/Main.java
@@ -1,39 +1,38 @@
 package AlarmClock;
 
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.time.*;
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
-
-class thread implements Runnable {
-    int point;
-    public thread(int a) {
-    this.point = a;
-    }
-
-    @Override
-    public void run() {
-        for (int i = point; i>= 1; i--){
-            System.out.println(i);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-}
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        boolean con = true;
+        LocalTime lt = null;
+        String wavFilePath = "C:\\Users\\home4\\Intelli_java\\Stealth_JAVA\\src\\AlarmClock\\audio.wav";
 
-        System.out.println("This is a alarm clock");
-        System.out.println("type the time to get started.");
-        int start = scanner.nextInt();
-        Thread t1 = new Thread(new thread(start));
-        t1.start();
+        Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+        System.out.println("This is a Alarm Clock!");
+
+        while (lt == null){
+            try {
+
+                System.out.print("Enter the Time now (HH:MM:SS): ");
+                String inputTime = scanner.nextLine();
+
+                lt = LocalTime.parse(inputTime, dtf);
+                System.out.println("Alarm is set for: " + lt);
+            } catch (DateTimeParseException e){
+                System.out.println("Invalid Time input");
+            }
+
+        }
+            AlarmClock alarmClock = new AlarmClock(lt, wavFilePath);
+            Thread thread0 = new Thread(alarmClock);
+            thread0.start();
 
         scanner.close();
     }
--- src/DateAndTime.java
+++ src/DateAndTime.java
@@ -7,13 +7,16 @@ public class DateAndTime {
     public static void main(String[] args) {
         LocalDate localDate = LocalDate.now();
         LocalDateTime dateTime = LocalDateTime.now();
-        
+
         System.out.println(localDate);
         System.out.println(dateTime);
 
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd:MM:yyyy HH:mm:ss");
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("MM:yyyy");
         String formatted = dateTime.format(dtf);
 
+        String formattedLocalDate = localDate.format(df);
+        System.out.println(formattedLocalDate);
         System.out.println(formatted);
     }
 }
