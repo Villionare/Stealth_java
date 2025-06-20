68adbd051faaea5da8e09b19650f95102cec34ac
duedull <mr.duedull@gmail.com>
2025-06-20 23:42:14 +0530

    Readling the first JSON file

--- src/FileHandling/Main.java
+++ src/FileHandling/Main.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 
 public class Main {
@@ -17,25 +18,26 @@ public class Main {
 //        InputStream IS = new InputStream();
 //        File file = new File("first.txt");
 
-        File first = new File("src\\FileHandling\\third.txt");
-        System.out.println(first.canRead());
-        System.out.println(first.canWrite());
-        System.out.println(first.exists());
-        System.out.println(first.getName());
-        System.out.println(first.getAbsolutePath());
-        System.out.println(first.getAbsolutePath());
-        System.out.println(first.length());
-        System.out.println(first.mkdir());
-        System.out.println(first.createNewFile());
+        try{
+            FileReader fr = new FileReader("src\\FileHandling\\START.json");
+            Scanner scanner = new Scanner(fr);
+             while (scanner.hasNextLine()){
+                 String data = scanner.nextLine();
+                 System.out.println(data);
+             }
+             scanner.close();
+             fr.close();
+        } catch (IOException e){
+            System.out.println(e);
+        }
 
-
-        FileWriter fw = new FileWriter("src\\FileHandling\\first.txt");
-        fw.append("this is the Appended text");
-
-        FileReader fr = new FileReader("src\\FileHandling\\first.txt");
-        System.out.println(fr.read());
-
-        fr.close();
-        fw.close();
+//        FileWriter fw = new FileWriter("src\\FileHandling\\first.txt");
+//        fw.append("this is the Appended text");
+//
+//        FileReader fr = new FileReader("src\\FileHandling\\first.txt");
+//        System.out.println(fr.read());
+//
+//        fr.close();
+//        fw.close();
     }
 }
