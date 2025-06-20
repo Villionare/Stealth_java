7473cbab65c56ff594f9aca1826dc02526bd0951
duedull <mr.duedull@gmail.com>
2025-06-20 23:18:58 +0530

    Tried some File methods

--- src/FileHandling/Main.java
+++ src/FileHandling/Main.java
@@ -1,6 +1,10 @@
 package FileHandling;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 public class Main {
     //Stream is a flow of data, like a sequence of bytes, from a source to a destination.
@@ -12,6 +16,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
 //        InputStream IS = new InputStream();
 //        File file = new File("first.txt");
+
+        File first = new File("src\\FileHandling\\third.txt");
+        System.out.println(first.canRead());
+        System.out.println(first.canWrite());
+        System.out.println(first.exists());
+        System.out.println(first.getName());
+        System.out.println(first.getAbsolutePath());
+        System.out.println(first.getAbsolutePath());
+        System.out.println(first.length());
+        System.out.println(first.mkdir());
+        System.out.println(first.createNewFile());
+
+
         FileWriter fw = new FileWriter("src\\FileHandling\\first.txt");
         fw.append("this is the Appended text");
 
